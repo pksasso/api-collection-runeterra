1fc1fc00a4cd6cddb905207f5a2145fdadf7062b
Patrick <patricksassorj@gmail.com>
2020-10-16 16:54:38 -0300

    ✨ Add deck code error

--- src/controllers/collectionController.js
+++ src/controllers/collectionController.js
@@ -6,8 +6,16 @@ const fullSet = require('../data/fullset-pt_br.json');
 module.exports = {
   async index (req, res) {
     const code = req.query.code;
-    const decodedDeck = deckDecoder(code);
+    let decodedDeck;
+
+    try{
+      decodedDeck = deckDecoder(code);
+    }catch(err){
+      return res.json({error: 'Invalid Code'});
+    }
+
     const result = collectionPlayerQuantity(decodedDeck);
+    
     return res.json(result)
   },
 } 
